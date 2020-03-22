c105b16cef70216b9c457e1c47166e6afb685042
Snow <singingai@hotmail.com>
2020-03-21 23:34:18 -0500

    4
    fds

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('button');
+
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
+}
+
+btn.onclick = function() {
+  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  document.body.style.backgroundColor = rndCol;
+}
+
+///Mozilla developer network event  - https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Building_blocks/Events////
